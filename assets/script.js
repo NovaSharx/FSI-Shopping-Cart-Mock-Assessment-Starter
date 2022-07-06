d99c7b62746d3aa84a4f8ad0978b93e0726105e0
Davin <davin-dj@hotmail.com>
2022-07-05 21:51:10 -0400

    Added total price to be displayed.

--- assets/script.js
+++ assets/script.js
@@ -5,14 +5,25 @@ for (let i = 0; i < cartItems.length; i++) {
     cartItem.index = i
     cartItem.totalQuantity = 1
     cartItem.totalQuantityText = cartItem.querySelector('.total-quantity')
+    cartItem.productInfo = cartItem.querySelector('.product-info')
     cartItem.addQuantityButton = cartItem.querySelector('#quantity-up')
     cartItem.subtractQuantityButton = cartItem.querySelector('#quantity-down')
     cartItem.removeButton = cartItem.querySelector('.remove')
+
+    cartItem.priceInfo = cartItem.productInfo.querySelector('P').textContent
+    cartItem.price = cartItem.priceInfo.split(' ')
+    cartItem.price = parseFloat(cartItem.price[0].slice(1))
+    cartItem.totalPrice = cartItem.querySelector(".total-price")
+    cartItem.totalPrice.append(document.createElement('p'))
+    cartItem.totalPrice.querySelector('p').textContent = "$" + cartItem.price
+
 
     cartItem.addQuantityButton.addEventListener("click", function(){
         console.log('Adding')
         cartItem.totalQuantity++
         cartItem.totalQuantityText.textContent = 'Quantity: '+ cartItem.totalQuantity
+        let currentTotal = cartItem.totalQuantity*cartItem.price
+        cartItem.totalPrice.querySelector('p').textContent = "$" + currentTotal
     })
 
     cartItem.subtractQuantityButton.addEventListener("click", function(){
@@ -23,6 +34,8 @@ for (let i = 0; i < cartItems.length; i++) {
         }
         else {
             cartItem.totalQuantityText.textContent = 'Quantity: '+ cartItem.totalQuantity
+            let currentTotal = cartItem.totalQuantity*cartItem.price
+            cartItem.totalPrice.querySelector('p').textContent = "$" + currentTotal
         }
     })
 
@@ -30,4 +43,7 @@ for (let i = 0; i < cartItems.length; i++) {
         let itemToRemove = cartItem.removeButton.parentElement
         itemToRemove.remove()
     })
+
+    console.log(cartItem.totalPrice.children)
+
 }
